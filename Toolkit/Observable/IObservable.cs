a49027fc267a5fba50367cde78113ce8bf01d2a1
Ole Wiedemann <61351036+w-syss@users.noreply.github.com>
2020-08-17 17:42:22 +0200

    Added ObservableObject and IObservable.

---
+++ Toolkit/Observable/IObservable.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace Toolkit.Observable
+{
+    public interface IObservable : INotifyPropertyChanged
+    {
+        ///
+        /// <summary>
+        /// Set the <paramref name="newValue"/> to the <paramref name="property"/>
+        /// and notify others of the change by invoking the <cref name="PropertyChanged"/> event.
+        /// </summary>
+        ///
+        /// <param name="property">Reference to the backing field of the property.</param>
+        /// <param name="newValue">The new value.</param>
+        /// <param name="propertyName">
+        /// Name of the changed property. This can be omitted, since the implementation
+        /// should resolve it via the compiler name service./>.
+        /// </param>
+        ///
+        void SetAndNotify<T>(ref T property, T newValue, string propertyName);
+    }
+}
---
+++ Toolkit/Observable/ObservableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Toolkit.Observable
+{
+    public class ObservableObject : IObservable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public virtual void SetAndNotify<T>(ref T property, T newValue, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(property, newValue))
+            {
+                return;
+            }
+
+            property = newValue;
+            NotifyPropertyChanged(propertyName);
+        }
+    }
+}
