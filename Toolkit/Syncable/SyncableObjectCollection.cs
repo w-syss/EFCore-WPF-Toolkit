43a0fb8a3b36d0b2a048b37cd261a9c03111d957
Ole Wiedemann <61351036+w-syss@users.noreply.github.com>
2020-08-20 22:04:54 +0200

    Added experimental RemoveFromDataStore. Increased version to 1.3.0.

--- Toolkit/Syncable/SyncableObjectCollection.cs
+++ Toolkit/Syncable/SyncableObjectCollection.cs
@@ -1,8 +1,22 @@
-﻿using Toolkit.Observable;
+﻿using System.Threading.Tasks;
+using Toolkit.DataStore;
+using Toolkit.Observable;
 
 namespace Toolkit.Syncable
 {
     public class SyncableObjectCollection<T> : ObservableObjectCollection<T> where T : class, ISyncableObject
     {
+
+        public async Task<bool> RemoveFromDataStore(T item)
+        {
+            using var context = DataContextHandler.CreateContext();
+            context.Remove(item);
+
+            var result = await context
+                .SaveChangesAsync()
+                .ConfigureAwait(false);
+
+            return result > 0;
+        }
     }
 }
